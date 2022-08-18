115e571678053c7583ad6f9100df80678080d5b5
sohab-abid99 <65116052+sohab-abid99@users.noreply.github.com>
2022-08-18 07:22:41 -0400

    added functions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-fetch('index.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-
+//another toggle function 
+$(".more").toggle(function () {
+    fetch('index.json')
+        .then((response) => response.json())
+        .then((json) => $(".load-content").html(json); $(".more").html("...Show Less"););
+}, function() {
+    $(".load-content").html(null);
+    $(".more").html("Show More...");
+});
