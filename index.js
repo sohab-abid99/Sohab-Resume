8feaadedab9cc630d9309fc6f4fba1eb044215d8
sohab-abid99 <65116052+sohab-abid99@users.noreply.github.com>
2022-08-18 11:18:04 -0400

    updated

--- index.js
+++ index.js
@@ -54,11 +54,28 @@ $(".more-tag3").toggle(function () {
             var li2 = document.createElement("li");
             li2.appendChild(document.createTextNode(json.content7));
             ul.appendChild(li2);
+        
+            var div2 = document.getElementsByClassName("certifications3");
+            var h5 = document.createElement("h5");
+            h5.appendChild(document.createTextNode(json.content8));
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(json.content9));
+
+            div2[0].appendChild(h5);
+            div2[0].appendChild(p);
+
+            var ul2 = document.getElementById("certifications3");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(json.content10));
+            ul2.appendChild(li);
+
+            $("#link1").remove();
             $(".more-tag3").html("...Show Less")
         });
 }, function () {
         $(".certifications2").html(null);
         $(".start-line").html(null);
         $("#certifications2").html(null);
+        $(".link-class").attr('id', 'link1');
     $(".more-tag3").html("Show More...");
 });
