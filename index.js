11cf37c48cb2a9a96f528ae6c7d70337f2af3eab
sohab-abid99 <65116052+sohab-abid99@users.noreply.github.com>
2022-08-18 06:31:48 -0400

    added js

---
+++ index.js
@@ -0,0 +1,28 @@
+﻿
+
+$(".more-tag").toggle(function () {
+    $("span").removeClass("show-more");
+    $(".more-tag").html("...Show Less");
+}, function () {
+        $(".content").addClass("show-more");
+        $(".more-tag").html("Show More...");
+});
+
+function readFile(event) {
+  //let file = input.files[0];
+
+  var selectedFile = event.target.files[0];
+    var reader = new FileReader();
+
+    var result = $(".load-content");
+
+    reader.onload = function(event) {
+      result.innerHTML = event.target.result;
+    };
+
+    reader.readAsText(selectedFile);
+}
+$(".more").click(function (event) {
+
+readFile(event);
+});
