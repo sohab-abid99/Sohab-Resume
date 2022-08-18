3a7fb442cd1391e8ca9f9621bb9fa5b1b388e051
sohab-abid99 <65116052+sohab-abid99@users.noreply.github.com>
2022-08-18 09:16:11 -0400

    added classes

--- index.js
+++ index.js
@@ -1,9 +1,63 @@
-//another toggle function 
-$(".more").toggle(function () {
+
+$(".more-tag1").toggle(function () {
     fetch('index.json')
         .then((response) => response.json())
-        .then((json) => $(".load-content").html(json.nam));
+        .then((json) => {
+            $(".content1").html(json.content1)
+            $(".more-tag1").html("...Show Less")
+        });
+}, function () {
+        $(".content1").html(null);
+        $(".more-tag1").html("Show More...");
+});
+
+//another toggle function 
+$(".more-tag2").toggle(function () {
+    fetch('index.json')
+        .then((response) => response.json())
+        .then((json) => {
+            $(".content2").html(json.content2)
+            $(".more-tag2").html("...Show Less")    
+        });
 }, function() {
-    $(".load-content").html(null);
-    $(".more").html("Show More...");
+    $(".content2").html(null);
+    $(".more-tag2").html("Show More...");
+});
+
+
+//another toggle function 
+$(".more-tag3").toggle(function () {
+    fetch('index.json')
+        .then((response) => response.json())
+        .then((json) => {
+            var div = document.getElementByClassName("certifications2");
+            var h5 = document.createElement("h5");
+            h5.appendChild(document.createTextNode(json.content3));
+
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(json.content4));
+
+            div.appendChild(p);
+            div.appendChild(h5);
+
+            var div = document.getElementByClassName("start-line");
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(json.content5));
+            div.appendChild(p);
+
+            var ul = document.getElementById("certifications2");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(json.content6));
+            ul.appendChild(li);
+
+            var li = document.createElement("certifications2");
+            li.appendChild(document.createTextNode(json.content7));
+            ul.appendChild(li);
+            $(".more-tag3").html("...Show Less")
+        });
+}, function () {
+        $(".certifications2").html(null);
+        $(".start-line").html(null);
+        $("#certifications2").html(null);
+    $(".more-tag3").html("Show More...");
 });
