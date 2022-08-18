cec2181040b63e27310d7d33eaeef246b76d2706
sohab-abid99 <65116052+sohab-abid99@users.noreply.github.com>
2022-08-18 07:39:16 -0400

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 $(".more").toggle(function () {
     fetch('index.json')
         .then((response) => response.json())
-        .then((json) => $(".load-content").html(json.name)); $(".more").html("...Show Less");
+        .then((json) => $(".load-content").html(json.nam));
 }, function() {
     $(".load-content").html(null);
     $(".more").html("Show More...");
