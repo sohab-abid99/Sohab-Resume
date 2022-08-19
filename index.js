6e8b60793b110c887e31ee7c1a69d670cd2f1315
sohab-abid99 <65116052+sohab-abid99@users.noreply.github.com>
2022-08-19 07:39:25 -0400

    updated

--- index.js
+++ index.js
@@ -67,45 +67,47 @@ $(".more-tag3").toggle(function () {
             var ul2 = document.getElementById("certifications3");
             var li = document.createElement("li");
             li.appendChild(document.createTextNode(json.content10));
-            ul2.appendChild(li);
-            $("#link1").removeClass("link1");
+            ul2[0].appendChild(li);
+
+            $("#link1").remove();
             $(".more-tag3").html("...Show Less")
-            
         });
-    
 }, function () {
         $(".certifications2").html(null);
         $(".start-line").html(null);
         $("#certifications2").html(null);
         $(".certifications3").html(null);
         $("#certifications3").html(null);
-        $("#link1").addClass("link1");
-    $(".more-tag3").html("Show More...");
+        $(".link-class").attr('id', 'link1');
+    $(".more-tag3").html("...Show Less");
 });
 
-//toggle function for web projects sections
+//toggle function for fyp
 $(".more-tag4").toggle(function () {
     fetch('index.json')
         .then((response) => response.json())
         .then((json) => {
-            $(".dice").html(json.content11);
-            var ul = document.getElementsByClassName("dice-list");
+            $(".fyp").html(json.content13);
+            var ul = document.getElementsByClassName("fyp-list");
             var li = document.createElement("li");
-            li.appendChild(document.createTextNode(json.content12));
-            ul[0].appendChild(li);   
-           var li2 = document.createElement("li");
-            li2.appendChild(document.createTextNode(json.content13));
+            li.appendChild(document.createTextNode(json.content11));
+            ul[0].appendChild(li);
+
+            var li2 = document.createElement("li");
+            li2.appendChild(document.createTextNode(json.content12));
             ul[0].appendChild(li2);
 
-            var a = document.createElement("a");
-            a.appendChild(document.createTextNode(json.content14));
-            li2.appendChild(a);
-           $(".more-tag4").html("...Show Less")
+           
+            
+            $(".more-tag4").html("...Show Less")
         });
     
+
+
+    
 }, function () {
-        $(".dice").html(null);
-        $(".dice-list").html(null);
-               
+        $(".fyp").html(null);
+        $(".fyp-list").html(null);
+       
         $(".more-tag4").html("Show More...");
 });
